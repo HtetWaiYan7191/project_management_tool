6e5598ca7580ecfd04fbdab36c8047095c7fb6b4
htetwaiyan <htetwaiyan7191@gmail.com>
2024-08-25 17:34:21 +0630

    add migration files

---
+++ db/migrate/20240825101638_create_annoucements.rb
@@ -0,0 +1,12 @@
+class CreateAnnoucements < ActiveRecord::Migration[7.1]
+  def change
+    create_table :annoucements do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :company, null: false, foreign_key: true
+      t.string :title
+      t.boolean :is_all_department, default: false
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20240825104525_create_doc_and_files.rb
@@ -0,0 +1,12 @@
+class CreateDocAndFiles < ActiveRecord::Migration[7.1]
+  def change
+    create_table :doc_and_files do |t|
+      t.references :company, null: false, foreign_key: true
+      t.references :department, null: false, foreign_key: true
+      t.string :title
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
